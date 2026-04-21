a63c8afdcf461da22a1a252025f16f0c33c28a84
kanchan sharma <kanusharma881388@gmail.com>
2026-04-21 18:14:59 +0530

    refactor(root): conditionally hide main navbar on seller dashboard routes

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,17 +1,23 @@
-
+"use client";
 import Navbar from '@/components/Navbar';
 import "./globals.css";
 import { ReduxProvider } from "@/redux/ReduxProvider";
 import Script from 'next/script';
 import { Toaster } from 'sonner';
 import AIAssistant from '../components/AIAssistant';
+import { usePathname } from 'next/navigation';
 
-export default function RootLayout({ children, }: { children: React.ReactNode; }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+
+  const isSellerPage = pathname.startsWith('/seller');
+
   return (
     <html lang="en">
       <body>
         <ReduxProvider>
-          <Navbar />
+          {!isSellerPage && <Navbar />}
+
           <main>
             <Toaster position="top-center" richColors closeButton />
             {children}
