74ccbde8ced563a5067ad673c7e550d9e0e2f318
kanchan sharma <kanusharma881388@gmail.com>
2026-05-05 12:38:36 +0530

    initial commit

--- frontend/src/app/order/[id]/page.tsx
+++ frontend/src/app/order/[id]/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useEffect, useState } from 'react';
 import { useParams } from 'next/navigation';
-import Link from 'next/link';
 
 export default function OrderDetailsPage() {
   const { id } = useParams();
