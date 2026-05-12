27be3350184e64d54086a8418845016568ecbe6b
kanchan sharma <kanusharma881388@gmail.com>
2026-05-12 23:48:22 +0530

    initial commit

--- frontend/src/app/seller/analytics/page.tsx
+++ frontend/src/app/seller/analytics/page.tsx
@@ -9,7 +9,7 @@ const AnalyticsPage = () => {
   ];
 
   return (
-    <div className="p-6 bg-black min-h-screen text-white">
+    <div className=" bg-black h-full text-white">
       <h1 className="text-3xl font-bold mb-8 italic">STORE <span className="text-blue-600">ANALYTICS</span></h1>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-10">
